96cfa47bd5a1154fc35410ee3f7f6a8f544417f8
820095P <820095@unizar.es>
2025-02-23 17:22:59 +0100

    primeras lineas del servidor

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 
 // PRIMERAS PRUEEBAS DEL SERVIDOR CON NODEJS Y EXPRESS
+
+//******PRIMERAS LINEAS DEL SERVIDOR  *********/
 const express = require('express');
 const cors = require('cors');
+
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess.js');
+
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Crea el servidor manualmente para poder utilizar WebSockets
+const server = http.createServer(app);
+const io = socket(server);
 
 // Ruta de prueba
 app.get("/", (req, res) => {
@@ -20,3 +31,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+// WebSockets para comprobar conexiones de usuarios
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+//*******IMPLEMENTACION LOGICA DE JUEGO DEL AJEDREZ *************/
