cb6a7cba4fee57c460c96ce989effeb638453e5f
hugo-lz <875455@unizar.es>
2025-02-26 13:30:09 +0100

    tabla retos

---
+++ create_table_reto.sql
@@ -0,0 +1,9 @@
+-- Tabla: Reto
+CREATE TABLE if NOT EXISTS Reto (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    Retador INT REFERENCES Usuario(id),
+    Retado INT REFERENCES Usuario(id),
+    Activo BOOLEAN DEFAULT TRUE,
+    Pendiente BOOLEAN DEFAULT TRUE
+);
--- create_tables.sql
+++ create_tables.sql
@@ -50,12 +50,4 @@ CREATE TABLE Amistad (
     Retos INT
 );
 
--- Tabla: Reto
-CREATE TABLE Reto (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    Retador INT REFERENCES Usuario(id),
-    Retado INT REFERENCES Usuario(id),
-    Activo BOOLEAN DEFAULT TRUE,
-    Pendiente BOOLEAN DEFAULT TRUE
-);
+
