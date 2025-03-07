258686da8e751b008ca138a96319e6a8201c7000
hugo-lz <875455@unizar.es>
2025-03-07 22:07:46 +0100

    cambio en mensaje

--- db/schemas/mensaje.js
+++ db/schemas/mensaje.js
@@ -2,14 +2,14 @@ import { sql } from 'drizzle-orm'
 import { timestamp } from 'drizzle-orm/gel-core'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
-
+import { usuario } from './usuario.js'
 
 
 export const mensaje = sqliteTable('mensaje', {
     Id_mensaje: integer('id_mensaje').primaryKey(),
     fecha_envio: timestamp('fecha_envio').default(sql`cast(strftime('%s', 'now') as int) * 1000`),
-    Id_partida: integer('id_partida'),
-    Id_usuario: integer('id_usuario'),
+    Id_partida: integer('id_partida').references(() => usuario.id).notNull(),
+    Id_usuario: integer('id_usuario').references(() => usuario.id).notNull(),
 
     Mensaje: text('mensaje').notNull()
 })
