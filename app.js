cccf2bd750a6dfedd543e17b6bf04978ea112a95
820095P <820095@unizar.es>
2025-03-14 23:19:55 +0100

    Deshaciendo cambio de puerto (no era eso)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { buscarUsuarioPorUser, buscarPartidasDeUsuario, buscarUlt5PartidasDeUsua
 
 export const app = express()
 
-app.set('port', process.env.PORT || 8080)
+app.set('port', process.env.PORT || 3000)
 app.set('trust proxy', true)
 
 app.use(cors())
