9d8fee01961c701381fb58a030ae2d03b6dcdf7a
jorgeruuiz <jorgeruiz2002@hotmail.es>
2025-03-20 03:24:32 +0100

    para que haga logout tb el cliente de prueba

--- clientLogin.js
+++ clientLogin.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const loginUrl = 'http://localhost:3000/login';  // URL de tu API de login
-const socketUrl = 'http://localhost:3000';     // URL del servidor WebSocket
+const loginUrl = 'http://localhost:3000/login';     // URL de tu API de login
+const logoutUrl = 'http://localhost:3000/logout';   // URL de tu API de logout
+const socketUrl = 'http://localhost:3000';          // URL del servidor WebSocket
 
+let socket;
 // Función para hacer login y luego conectarse al WebSocket
 async function clientLogin(username, password) {
   try {
@@ -19,7 +21,7 @@ async function clientLogin(username, password) {
     console.log('Login exitoso. Token recibido:', token);
 
     // Conectar al servidor WebSocket usando el token
-    const socket = io(socketUrl, {
+    socket = io(socketUrl, {
       query: { token: token }  // Enviar el token a través del query en la conexión
     });
 
@@ -37,8 +39,25 @@ async function clientLogin(username, password) {
   }
 }
 
+async function clientLogout(username) {
+    try {
+        // Realiza la petición POST para hacer login
+        await axios.post(logoutUrl, {
+            NombreUser: username,
+        });
+    
+        console.log('Logout exitoso. Adiós!');
+        socket.disconnect();
+    } catch (error) {
+        console.error('Error al hacer logout:', error.message);
+    }
+}
+
 // Intenta hacer login y conectarse con un usuario y contraseña
 const username = 'sammy';  // Sustituir con el nombre de usuario
 const password = '123456';  // Sustituir con la contraseña
 
 clientLogin(username, password);
+setTimeout(() => {
+  clientLogout(username);
+}, 5000); // Desconectar después de 5 segundos
